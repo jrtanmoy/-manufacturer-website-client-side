fb3d4058f72b1d8fc898928a6b2de9acbfb3748a
jrtanmoy <roy28301@gmail.com>
2022-05-21 22:12:19 +0600

    Navber added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Navbar from './Pages/Shared/Navber';
 
 function App() {
   return (
     <div>
-      <button class="btn btn-primary">Button</button>
+      <Navbar></Navbar>
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
   );
 }
