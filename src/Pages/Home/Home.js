5c03b6d076810778eae57b19f83e77bbe6b4187e
jrtanmoy <roy28301@gmail.com>
2022-05-22 01:20:49 +0600

    review section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from '../Shared/Footer';
 import Banner from './Banner';
 import BusinessSummary from './BusinessSummary';
+import Testimonials from './Testimonials.js';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <BusinessSummary></BusinessSummary>
+            <Testimonials></Testimonials>
             <Footer></Footer>
         </div>
     );
