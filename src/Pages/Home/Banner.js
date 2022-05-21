4ad8142617eb569ddca8caae696b183c7fc39dc0
jrtanmoy <roy28301@gmail.com>
2022-05-22 01:06:45 +0600

    business summary added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
     return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={banner} className="max-w-lg rounded-lg shadow-2xl" />
+                <img src={banner} className="max-w-lg rounded-lg shadow-2xl" alt='' />
                 <div>
                     <h1 className="text-5xl font-bold">Your New Tool Starts Here</h1>
                     <p className="py-6">They won’t all fit in your toolbox, but these are the essential tools you need for all sorts of repair jobs, DIY projects, and general upkeep.</p>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../Shared/Footer';
 import Banner from './Banner';
+import BusinessSummary from './BusinessSummary';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <BusinessSummary></BusinessSummary>
             <Footer></Footer>
         </div>
     );
