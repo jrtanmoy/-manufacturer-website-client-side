3b4a31156c408aaf75346e354c6f7d9dc354fb99
jrtanmoy <roy28301@gmail.com>
2022-05-23 20:23:01 +0600

    update form  on purchase page

--- src/Pages/Home/ToolDetail.js
+++ src/Pages/Home/ToolDetail.js
@@ -19,13 +19,20 @@ const ToolDetail = () => {
             .then(data => setTool(data));
     }, [tool])
 
-    const handleUpdateRestock = event => {
+    const handlePurchase = event => {
         event.preventDefault();
-        
-        const restockquantity = parseInt(event.target.quantity.value);
-        console.log(restockquantity);
-        
-        
+
+        const item = {
+            name: user.displayName,
+            email: user.email,
+            address: event.target.address.value,
+            phone: event.target.phone.value,
+            quantity: event.target.quantity.value,
+
+        }
+        console.log(item);
+
+
     }
 
 
@@ -34,38 +41,41 @@ const ToolDetail = () => {
         <div>
             <h2 className='text-4xl text-secondary text-center font-bold my-8'>Welcome to detail</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-16'>
-            <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-                <figure><img src={tool.img} alt="Shoes" /></figure>
-                <div class="card-body mt-5">
-                    <h2 class="card-title text-secondary">{tool.name}</h2>
-                    <p><span className='font-bold'>Price</span> : ${tool.price} /per unit</p>
-                    <p><span className='font-bold'>Minimum Order Quantity : {tool.minimumQuantity} unit</span></p>
-                    <p><span className='font-bold'>Available Order Quantity : {tool.availableQuantity} unit</span></p>
-                    <p><span className='font-bold'>Description</span> : {tool.description}</p>
-                </div>
-            </div>
-
-            <div className=' text-center justify-items-center mt-4 mb-5 '>
-                <div>
-                    <span className='text-2xl font-bold'>Your Name : {user.displayName}</span>
-                    <br />
-                    <span className='text-2xl font-bold'>Your Email : {user.email}</span>
-                </div>
-                <div className='text-center'>
-                    <div className='mb-4'>
-                        <form className='mt-6' onSubmit={handleUpdateRestock}>
-                            <h5 className='text-xl mb-6'>Your Order Quantity:</h5>
-                            <div className='flex'>
-
-                                <input className="input input-bordered w-6/12 max-w-xs" type="number" name="quantity"  placeholder='Quantity' />
-
-                                <input className="input input-bordered ml-6 w-3/12 max-w-xs" type="submit" value="Add" />
-                            </div>
-                        </form>
+                <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+                    <figure><img src={tool.img} alt="Shoes" /></figure>
+                    <div class="card-body mt-5">
+                        <h2 class="card-title text-secondary">{tool.name}</h2>
+                        <p><span className='font-bold'>Price</span> : ${tool.price} /per unit</p>
+                        <p><span className='font-bold'>Minimum Order Quantity : {tool.minimumQuantity} unit</span></p>
+                        <p><span className='font-bold'>Available Order Quantity : {tool.availableQuantity} unit</span></p>
+                        <p><span className='font-bold'>Description</span> : {tool.description}</p>
                     </div>
-
                 </div>
-            </div>
+
+                <div className=' text-center justify-items-center mb-5 '>
+                    <div className='text-center'>
+                        <div className=''>
+                            <h5 className='text-xl mb-6'>Please Purchase</h5>
+                            <form className='mt-6' onSubmit={handlePurchase}>
+
+                                <input className="input input-bordered w-full max-w-xs mt-1" type="text" value={user?.displayName} name="name" placeholder='name' required readOnly disabled />
+                                <br />
+                                <input className="input input-bordered w-full max-w-xs mt-1" type="email" value={user?.email} name="email" placeholder='email' required readOnly disabled />
+                                <br />
+                                <input className="input input-bordered w-full max-w-xs mt-1" type="text" name="address" placeholder='Address'  />
+                                <br />
+                                <input className="input input-bordered w-full max-w-xs mt-1" type="text" name="phone" placeholder="Phone Number"  />
+                                <br />
+                                <input className="input input-bordered w-full max-w-xs mt-1" type="number" name="quantity" placeholder='Quantity' />
+                                <br />
+
+                                <input className="input input-bordered ml-6 w-3/12 max-w-xs mt-2" type="submit" value="Purchase" />
+
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
             </div>
 
 
