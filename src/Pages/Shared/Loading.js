b1bffc3daac9bbaab0d1018691a2bead693cf205
jrtanmoy <roy28301@gmail.com>
2022-05-23 11:32:40 +0600

    handle login error, created & show loading

---
+++ src/Pages/Shared/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div class="flex items-center h-screen justify-center ">
+            <button class="btn loading">loading</button>
+        </div>
+    );
+};
+
+export default Loading;
