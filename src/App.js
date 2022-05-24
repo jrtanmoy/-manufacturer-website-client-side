b815c2040ddb5d31eb67feb0ef80c555108f9de6
jrtanmoy <roy28301@gmail.com>
2022-05-24 22:29:56 +0600

    create require admin to protect admin only route

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import MyOrders from './Pages/Dashboard/MyOrders';
 import MyReview from './Pages/Dashboard/MyReview';
 import MyProfile from './Pages/Dashboard/MyProfile';
 import Users from './Pages/Dashboard/Users';
+import RequireAdmin from './Pages/Login/RequireAdmin';
 
 function App() {
   return (
@@ -30,7 +31,7 @@ function App() {
             <Route index element={<MyOrders></MyOrders>}></Route>
             <Route path='review' element={<MyReview></MyReview>}></Route>
             <Route path='profile' element={<MyProfile></MyProfile>}></Route>
-            <Route path='users' element={<Users></Users>}></Route>
+            <Route path='users' element={<RequireAdmin><Users></Users></RequireAdmin>}></Route>
           </Route>
         <Route path='/about' element={<About />} />
         <Route path='/blog' element={<Blogs />} />
