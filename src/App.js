75b94414b2118d5f1c31cd4a89059a7c207a92fc
jrtanmoy <roy28301@gmail.com>
2022-05-24 11:44:47 +0600

    update my order page for cancel

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Dashboard from './Pages/Dashboard/Dashboard';
 import MyOrders from './Pages/Dashboard/MyOrders';
 import MyReview from './Pages/Dashboard/MyReview';
+import MyProfile from './Pages/Dashboard/MyProfile';
 
 function App() {
   return (
@@ -23,16 +24,11 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/purchase/:toolId'
-          element={
-            <RequireAuth><ToolDetail /></RequireAuth>
-          } />
-        <Route path='/dashboard'
-          element={
-            <RequireAuth><Dashboard /></RequireAuth>
-          } >
+        <Route path='/purchase/:toolId' element={<RequireAuth><ToolDetail /></RequireAuth>} />
+        <Route path='/dashboard' element={<RequireAuth><Dashboard /></RequireAuth>} >
             <Route index element={<MyOrders></MyOrders>}></Route>
             <Route path='review' element={<MyReview></MyReview>}></Route>
+            <Route path='profile' element={<MyProfile></MyProfile>}></Route>
           </Route>
         <Route path='/about' element={<About />} />
         <Route path='/blog' element={<Blogs />} />
--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 
 const MyOrders = () => {
     const [purchases, setPurchases] = useState([]);
     const [user] = useAuthState(auth);
+
 
     useEffect(() => {
         if (user) {
@@ -12,7 +14,23 @@ const MyOrders = () => {
                 .then(res => res.json())
                 .then(data => setPurchases(data));
         }
-    }, [user])
+    })
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure?');
+        if (proceed) {
+            fetch(`http://localhost:5000/purchase/${id}`, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount) {
+                        toast.success(`your order for this item deleted.`)
+                    }
+                })
+        }
+    }
 
     return (
         <div>
@@ -23,19 +41,22 @@ const MyOrders = () => {
                         <tr>
                             <th></th>
                             <th>Name</th>
+                            <th>Item Name</th>
                             <th>Quantity</th>
                             <th>Price</th>
-                            <th>Address</th>
+                            <th>Cancel Item</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            purchases.map(a => <tr>
-                                <th>1</th>
+                            purchases.map((a, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{a.name}</td>
                                 <td>{a.productname}</td>
+                                {/* <td>{a._id}</td> */}
                                 <td>{a.quantity}</td>
                                 <td>{a.price}</td>
-                                <td>{a.address}</td>
+                                <td><button onClick={() => handleDelete(a._id)} class="btn btn-xs btn-error">Cancel</button></td>
                             </tr>)
                         }
 
