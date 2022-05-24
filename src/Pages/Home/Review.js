bb655e7c8c752e03e5514a98a1d37a0114aa14e9
jrtanmoy <roy28301@gmail.com>
2022-05-24 13:11:34 +0600

    display review from database

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -4,16 +4,17 @@ const Review = ({ review }) => {
     return (
         <div className="card lg:max-w-lg bg-base-100 shadow-xl">
             <div className="card-body">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, voluptates quo incidunt harum minus mollitia!</p>
                 <div className="flex items-center">
-                    <div className="avatar">
-                        <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
-                            <img src={review.img} alt=""/>
-                        </div>
-                    </div>
                     <div>
-                        <h4 className='text-xl'>{review.name}</h4>
-                        <p>{review.location}</p>
+                        <h4 className='text-xl text-secondary text-center font-bold'>Name: {review.name}</h4>
+                        <p className='text-md font-bold flex'>Ratting: {review.ratting}
+                            <span className='text-amber-500 mt-1'>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                </svg>
+                            </span>
+                        </p>
+                        <p className='mt-2'>{review.description}</p>
                     </div>
                 </div>
             </div>
--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -1,33 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import quote from '../../assets/icons/quote.svg';
-import people1 from '../../assets/images/people1.png';
-import people2 from '../../assets/images/people2.png';
-import people3 from '../../assets/images/people3.png';
 import Review from './Review';
 const Testimonials = () => {
-    const reviews = [
-        {
-            _id: 1,
-            name: 'Winson Herry',
-            review: '',
-            location: 'california',
-            img: people1
-        },
-        {
-            _id: 2,
-            name: 'Winson Herry',
-            review: '',
-            location: 'california',
-            img: people2
-        },
-        {
-            _id: 3,
-            name: 'Winson Herry',
-            review: '',
-            location: 'california',
-            img: people3
-        },
-    ];
+
+    const [reviews, setReviews] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/review')
+            .then(res => res.json())
+            .then(data => setReviews(data));
+    }, [])
+    
     return (
         <section className='my-28'>
             <div className='flex justify-between'>
