f09e3b5a9526f0ac12b351fe09abfa361f22db3b
jrtanmoy <roy28301@gmail.com>
2022-05-24 12:46:16 +0600

    update my review page to post review

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -6,12 +6,12 @@ const Dashboard = () => {
         <div class="drawer drawer-mobile">
         <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
         <div class="drawer-content">
-            <h2 className='text-2xl font-bold text-purple-500'>Welcome to your Dashboard</h2>
+            <h2 className='text-4xl font-bold text-purple-500 text-center'>Welcome to your Dashboard</h2>
             <Outlet></Outlet>
         </div>
         <div class="drawer-side">
             <label for="dashboard-sidebar" class="drawer-overlay"></label>
-            <ul class="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
+            <ul class="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content mt-4">
                     {/* <!-- Sidebar content here --> */}
                     <li><Link to="/dashboard">My Orders</Link></li>
                     <li><Link to="/dashboard/review">My Reviews</Link></li>
--- src/Pages/Dashboard/MyReview.js
+++ src/Pages/Dashboard/MyReview.js
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
 
 const MyReview = () => {
+
+    const [user] = useAuthState(auth);
+    const [isDisabled, setDisabled] = useState(false);
+
+    const handlePurchase = event => {
+        event.preventDefault();
+
+        const rattingQuantity = parseInt(event.target.ratting.value)
+        console.log(rattingQuantity);
+
+        if((0 >= rattingQuantity) || (rattingQuantity >= 6)){
+            toast(`you have to ratting between 1 to 5 quantity`)
+            setDisabled(true);
+            event.target.reset();
+            return;
+        }
+
+
+
+
+        const review = {
+            name: user.displayName,
+            email: user.email,
+            ratting: event.target.ratting.value,
+            description: event.target.description.value,
+
+        }
+        console.log(review);
+        fetch('http://localhost:5000/review', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    toast('Your review is successfully submitted!!');
+                    event.target.reset();
+                }
+            })
+
+
+    }
+
     return (
         <div>
-            <h1>my review</h1>
+            <h2 className='text-2xl text-secondary text-center font-bold my-8'>Want to give us review!!</h2>
+
+
+
+            <div className=' text-center justify-items-center mb-5 '>
+                <div className='text-center'>
+                    <div className=''>
+                        <h5 className='text-xl mb-6'>Please Review</h5>
+                        <form className='mt-6' onSubmit={handlePurchase}>
+                            <input className="input input-bordered w-full max-w-xs mt-1" type="text" value={user?.displayName} name="name" placeholder='name' required readOnly disabled />
+                            <br />
+                            <input className="input input-bordered w-full max-w-xs mt-1" type="email" value={user?.email} name="email" placeholder='email' required readOnly disabled />
+                            <br />
+                            <input className="input input-bordered w-full max-w-xs mt-1" type="number"
+                            name="ratting" placeholder='Ratting (should be 1 to 5)' required />
+                            <br />
+                            <input className="input input-bordered w-full max-w-xs mt-1" type="text" name="description" placeholder='Description' required />
+                            <br />
+
+                            <button className="btn mt-2" type='submit' disabled={isDisabled}>Add</button>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+
         </div>
     );
 };
