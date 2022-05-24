cc835e21ea59094fb4e542e351842214c8ef93e4
jrtanmoy <roy28301@gmail.com>
2022-05-24 13:28:49 +0600

    some update to testimonial page

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -6,9 +6,9 @@ const Review = ({ review }) => {
             <div className="card-body">
                 <div className="flex items-center">
                     <div>
-                        <h4 className='text-xl text-secondary text-center font-bold'>Name: {review.name}</h4>
-                        <p className='text-md font-bold flex'>Ratting: {review.ratting}
-                            <span className='text-amber-500 mt-1'>
+                        <h4 className='text-xl text-secondary  font-bold'>Name: {review.name}</h4>
+                        <p className='text-md font-bold flex my-2'>Ratting: {review.ratting}
+                            <span className='text-amber-500 pt-1 ml-1'>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                                 </svg>
