df25571f66af8a7cadc2765c2300faa2652ed733
jrtanmoy <roy28301@gmail.com>
2022-05-25 13:00:20 +0600

    simple credit card form setup

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,20 +1,25 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51L3DomCI5ofJBBGayDjJS74k5iSYlvSs20eRm5VZjFMVU7Mw3exS4D5bFsAb85iFvdivget0w5FIE8izunuak5FO00pqqKTVIA');
 
 const Payment = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const url = `http://localhost:5000/purchase/${id}`;
 
-    const {data: order, isLoading} = useQuery(['purchase', id], () => fetch(url,  {
+    const { data: order, isLoading } = useQuery(['purchase', id], () => fetch(url, {
         method: 'GET',
         headers: {
             'authorization': `Bearer ${localStorage.getItem('accessToken')}`
         }
-    }).then(res=>res.json()));
+    }).then(res => res.json()));
 
-    if(isLoading){
+    if (isLoading) {
         return <Loading></Loading>
     }
 
@@ -32,7 +37,9 @@ const Payment = () => {
             </div>
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div class="card-body">
-
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
                 </div>
             </div>
         </div>
